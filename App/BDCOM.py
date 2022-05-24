87aa0894660f07398879691daec6cb37154f2eba
Yukirona <81645346+Yukirona@users.noreply.github.com>
2022-05-24 14:19:42 +0200

    update

--- App/BDCOM.py
+++ App/BDCOM.py
@@ -4,6 +4,8 @@ import json
 from local_data import get_local_data
 import pandas as pd
 
+# - Function to retrieve the dataset contained in the BDCOM database
+
 def INDICS_BDCOM():
     
         jeu_donnees = "BDCOM2018"
@@ -14,15 +16,14 @@ def INDICS_BDCOM():
 
         wanted_data_BDCOM = get_local_data(jeu_donnees ,croisement ,modalités ,nivgeo ,codgeo)
 
+        #with open("wanted_data_BDCOM.json", "w") as outfile:
+                #json.dump(wanted_data_BDCOM, outfile)
 
-
-        with open("wanted_data_BDCOM.json", "w") as outfile:
-                json.dump(wanted_data_BDCOM, outfile)
-
-        print(type(wanted_data_BDCOM))
+        #print(type(wanted_data_BDCOM))
 
         return (wanted_data_BDCOM)
 
+# - Function to exract data from the dataset and create the POP_T1 array
 def POP_T1(wanted_data_BDCOM):
 
         pop1968 = (wanted_data_BDCOM['Cellule'][1]['Valeur'])
