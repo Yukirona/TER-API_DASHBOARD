78143788040f463fb94332425defb890617fe80b
Yukirona <81645346+Yukirona@users.noreply.github.com>
2022-06-03 09:12:26 +0200

    website

--- Flask/hello.py
+++ Flask/hello.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from api import init_dash
-
+import random
 
 app = Flask(__name__)
 
---
+++ Flask/static/navigation.js
@@ -0,0 +1,14 @@
+
+
+$('#nav1').on('click', function(){
+   $('#content').empty()
+
+});
+
+$('#nav2').on('click', function(){
+   $('#content').load('/Population');
+
+});
+
+
+
